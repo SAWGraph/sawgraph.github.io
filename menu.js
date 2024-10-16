4012232fb5f4b581c6ebee1a4bf875d5cebeb42e
kschweikert <kschweik@gmail.com>
2024-10-16 13:55:29 -0400

    Add second use case page

---
+++ menu.js
@@ -0,0 +1,24 @@
+document.write('\
+        <!-- Navbar (sit on top) -->\
+        <div class="w3-top">\
+            <div class="w3-bar w3-white w3-wide w3-padding w3-card">\
+            <a href="index.html#home" class="w3-bar-item w3-button"><b>SAW</b>Graph</a>\
+            <!-- Float links to the right. Hide them on small screens -->\
+            <div class="w3-right w3-hide-small">\
+                <a href="index.html#intro" class="w3-bar-item w3-button">Overview</a>\
+                <a href="index.html#about-pfas" class="w3-bar-item w3-button">PFAS</a>\
+                <a href="index.html#about-kgs" class="w3-bar-item w3-button">Knowledge Graphs</a>\
+                <div class="w3-dropdown-hover">\
+                    <button class="w3-bar-item w3-button w3-wide">Examples</button>\
+                    <div class="w3-dropdown-content w3-bar-block w3-card-4">\
+                        <a href="testing.html" class="w3-bar-item w3-button">Testing Gaps</a>\
+                        <a href="impacts.html" class="w3-bar-item w3-button">Assessing Impacts</a>\
+                        <a href="#" class="w3-bar-item w3-button">Contaminant Tracing</a>\
+                    </div>\
+                    </div>\
+                <a href="index.html#project" class="w3-bar-item w3-button">Project</a>\
+                <a href="index.html#team" class="w3-bar-item w3-button">Team</a>\
+            </div>\
+            </div>\
+        </div>\
+        ');
