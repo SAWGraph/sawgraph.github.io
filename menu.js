9b9a9d4f3e8ddbfd749abc3b69726db6873b607e
kschweikert <kschweik@gmail.com>
2026-01-09 16:07:17 -0500

    Split into separate pages.

--- menu.js
+++ menu.js
@@ -2,18 +2,35 @@ document.write('\
             <a href="index.html#home" class="w3-bar-item w3-button"><b>SAW</b>Graph</a>\
             <!-- Float links to the right. Hide them on small screens -->\
             <div class="w3-right w3-hide-small">\
-                <a href="index.html#intro" class="w3-bar-item w3-button">Overview</a>\
-                <a href="index.html#about-pfas" class="w3-bar-item w3-button">PFAS</a>\
-                <a href="index.html#about-kgs" class="w3-bar-item w3-button">Knowledge Graphs</a>\
-                <div class="w3-dropdown-hover">\
+            <a href="index.html" class="w3-bar-item w3-button"></a>\
+            <div class="w3-dropdown-hover">\
+                <button class="w3-bar-item w3-button w3-wide">Overview</button>\
+                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px">\
+                        <a href="index.html" class="w3-bar-item w3-button">Intro</a>\
+                        <a href="index.html#use_cases" class="w3-bar-item w3-button">Use Cases</a>\
+                        <a href="pfas.html" class="w3-bar-item w3-button">About PFAS</a>\
+                </div>\
+            </div>\
+                \
+            <div class="w3-dropdown-hover">\
+                <button class="w3-bar-item w3-button w3-wide">Technical Details</button>\
+                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px">\
+                    <a href="kg.html#project" class="w3-bar-item w3-button">Architecture</a>\
+                    <a href="kg.html#data-sources" class="w3-bar-item w3-button">Data Sources</a>\
+                    <a href="index.html#about-kgs" class="w3-bar-item w3-button">About KGs</a>\
+                </div>\
+            </div>\
+                \
+            <div class="w3-dropdown-hover">\
                     <button class="w3-bar-item w3-button w3-wide">Examples</button>\
                     <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px">\
                         <a href="testing.html" class="w3-bar-item w3-button">Testing Gaps</a>\
                         <a href="impacts.html" class="w3-bar-item w3-button">Assessing Impacts</a>\
                         <a href="tracing.html" class="w3-bar-item w3-button">Contaminant Tracing</a>\
                     </div>\
-                    </div>\
-                <a href="index.html#project" class="w3-bar-item w3-button">Data Sources</a>\
-                <a href="index.html#team" class="w3-bar-item w3-button">Team</a>\
+            </div>\
+                \
+            <a href="aboutus.html" class="w3-bar-item w3-button">Team</a>\
+            \
             </div>\
         ');
