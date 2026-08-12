e2ac04ffe17a109131812271ddfba3c2f9370ab7
Yadina Clark <yadina.clark@maine.edu>
2026-08-12 10:19:56 -0400

    Fix vertical menu change

--- menu.js
+++ menu.js
@@ -15,7 +15,7 @@ document.write(`
 
     <div
         id="siteMenu"
-        class="w3-white w3-card-4"
+        class="w3-bar-block w3-white w3-card-4"
         style="
             display:none;
             position:fixed;
@@ -29,106 +29,106 @@ document.write(`
 
         <!-- About SAWGraph -->
         <a href="index.html"
-           class="w3-bar-item w3-button w3-block w3-left-align">
+           class="w3-bar-item w3-button w3-left-align">
             <strong>About SAWGraph</strong>
         </a>
 
         <a href="index.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Overview
         </a>
 
         <a href="pfas.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             About PFAS
         </a>
 
         <!-- Data & Methods -->
         <a href="kg.html"
-           class="w3-bar-item w3-button w3-block w3-left-align">
+           class="w3-bar-item w3-button w3-left-align">
             <strong>Data &amp; Methods</strong>
         </a>
 
         <a href="kg.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Architecture
         </a>
 
         <a href="kg.html#data-sources"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Data Sources
         </a>
 
         <a href="ontologies.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Ontologies
         </a>
 
         <a href="aboutkgs.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             About KGs
         </a>
 
         <!-- Explore -->
-        <div class="w3-bar-item w3-block">
+        <div class="w3-bar-item">
             <strong>Explore</strong>
         </div>
 
         <a href="testing.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Testing Demo 1
         </a>
 
         <a href="testing_aquifers.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Testing Demo 2
         </a>
 
         <a href="impacts.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Impacts Demo 1
         </a>
 
         <a href="tracing.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Tracing Demo 1
         </a>
 
         <a href="tracing_alldownstream.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Tracing Demo 2
         </a>
 
         <a href="tracing_upstream.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Tracing Demo 3
         </a>
 
         <!-- Project -->
-        <div class="w3-bar-item w3-block">
+        <div class="w3-bar-item">
             <strong>Project</strong>
         </div>
 
         <a href="aboutus.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Team
         </a>
 
         <a href="publications.html"
-           class="w3-bar-item w3-button w3-block w3-left-align"
+           class="w3-bar-item w3-button w3-left-align"
            style="padding-left:32px;">
             Publications
         </a>
