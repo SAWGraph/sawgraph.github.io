ea021d90d9e305405246a608c8fc7546331f6021
Yadina Clark <yadina.clark@maine.edu>
2026-08-12 10:13:01 -0400

    Implement initial menu changes

--- menu.js
+++ menu.js
@@ -1,41 +1,182 @@
-document.write('\
-            <a href="index.html#home" class="w3-bar-item w3-button"><b>SAW</b>Graph</a>\
-            <!-- Float links to the right. Hide them on small screens -->\
-            <div class="w3-right w3-hide-small">\
-            <a href="index.html" class="w3-bar-item w3-button"> </a>\
-            <div class="w3-dropdown-hover">\
-                <button class="w3-bar-item w3-button w3-wide">Overview</button>\
-                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px">\
-                        <a href="index.html" class="w3-bar-item w3-button">Intro</a>\
-                        <a href="index.html#use_cases" class="w3-bar-item w3-button">Use Cases</a>\
-                        <a href="pfas.html" class="w3-bar-item w3-button">About PFAS</a>\
-                </div>\
-            </div>\
-                \
-            <div class="w3-dropdown-hover">\
-                <button class="w3-bar-item w3-button w3-wide">Technical Details</button>\
-                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px">\
-                    <a href="kg.html#project" class="w3-bar-item w3-button">Architecture</a>\
-                    <a href="kg.html#data-sources" class="w3-bar-item w3-button">Data Sources</a>\
-                    <a href="ontologies.html" class="w3-bar-item w3-button">Ontologies</a>\
-                    <a href="aboutkgs.html" class="w3-bar-item w3-button">About KGs</a>\
-                    <a href="publications.html" class="w3-bar-item w3-button">Publications</a>\
-                </div>\
-            </div>\
-                \
-            <div class="w3-dropdown-hover">\
-                    <button class="w3-bar-item w3-button w3-wide">Examples</button>\
-                    <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 32px; z-index:9999;">\
-                        <a href="testing.html" class="w3-bar-item w3-button">Testing: Waterbodies Near Facilities </a>\
-                        <a href="testing_aquifers.html" class="w3-bar-item w3-button">Testing: Wells with Common Aquifers</a>\
-                        <a href="impacts.html" class="w3-bar-item w3-button">Assessing Impacts</a>\
-                        <a href="tracing.html" class="w3-bar-item w3-button">Tracing:Downstream of Facilities</a>\
-                        <a href="tracing_alldownstream.html" class="w3-bar-item w3-button">Tracing:Downstream of Industry Facilities</a>\
-                        <a href="tracing_upstream.html" class="w3-bar-item w3-button">Tracing:Upstream of Contamination</a>\
-                    </div>\
-            </div>\
-                \
-            <a href="aboutus.html" class="w3-bar-item w3-button">Team</a>\
-            \
-            </div>\
-        ');
+document.write(`
+    <a href="index.html#home" class="w3-bar-item w3-button">
+        <b>SAW</b>Graph
+    </a>
+
+    <button
+        id="siteMenuButton"
+        class="w3-bar-item w3-button w3-right"
+        onclick="toggleSiteMenu(event)"
+        aria-label="Open navigation menu"
+        aria-expanded="false"
+        aria-controls="siteMenu">
+        &#9776;
+    </button>
+
+    <div
+        id="siteMenu"
+        class="w3-white w3-card-4"
+        style="
+            display:none;
+            position:fixed;
+            right:16px;
+            width:340px;
+            max-width:calc(100vw - 32px);
+            max-height:calc(100vh - 80px);
+            overflow-y:auto;
+            z-index:9999;
+        ">
+
+        <!-- About SAWGraph -->
+        <a href="index.html"
+           class="w3-bar-item w3-button w3-block w3-left-align">
+            <strong>About SAWGraph</strong>
+        </a>
+
+        <a href="index.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Overview
+        </a>
+
+        <a href="pfas.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            About PFAS
+        </a>
+
+        <!-- Data & Methods -->
+        <a href="kg.html"
+           class="w3-bar-item w3-button w3-block w3-left-align">
+            <strong>Data &amp; Methods</strong>
+        </a>
+
+        <a href="kg.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Architecture
+        </a>
+
+        <a href="kg.html#data-sources"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Data Sources
+        </a>
+
+        <a href="ontologies.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Ontologies
+        </a>
+
+        <a href="aboutkgs.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            About KGs
+        </a>
+
+        <!-- Explore -->
+        <div class="w3-bar-item w3-block">
+            <strong>Explore</strong>
+        </div>
+
+        <a href="testing.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Testing Demo 1
+        </a>
+
+        <a href="testing_aquifers.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Testing Demo 2
+        </a>
+
+        <a href="impacts.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Impacts Demo 1
+        </a>
+
+        <a href="tracing.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Tracing Demo 1
+        </a>
+
+        <a href="tracing_alldownstream.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Tracing Demo 2
+        </a>
+
+        <a href="tracing_upstream.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Tracing Demo 3
+        </a>
+
+        <!-- Project -->
+        <div class="w3-bar-item w3-block">
+            <strong>Project</strong>
+        </div>
+
+        <a href="aboutus.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Team
+        </a>
+
+        <a href="publications.html"
+           class="w3-bar-item w3-button w3-block w3-left-align"
+           style="padding-left:32px;">
+            Publications
+        </a>
+
+    </div>
+`);
+
+function toggleSiteMenu(event) {
+    event.stopPropagation();
+
+    const menu = document.getElementById("siteMenu");
+    const button = document.getElementById("siteMenuButton");
+    const navbar = button.closest(".w3-bar");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        button.setAttribute("aria-expanded", "false");
+    } else {
+        const navbarBottom = navbar.getBoundingClientRect().bottom;
+        menu.style.top = navbarBottom + "px";
+        menu.style.display = "block";
+        button.setAttribute("aria-expanded", "true");
+    }
+}
+
+document.addEventListener("click", function(event) {
+    const menu = document.getElementById("siteMenu");
+    const button = document.getElementById("siteMenuButton");
+
+    if (
+        menu &&
+        button &&
+        !menu.contains(event.target) &&
+        !button.contains(event.target)
+    ) {
+        menu.style.display = "none";
+        button.setAttribute("aria-expanded", "false");
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        const menu = document.getElementById("siteMenu");
+        const button = document.getElementById("siteMenuButton");
+
+        if (menu && button) {
+            menu.style.display = "none";
+            button.setAttribute("aria-expanded", "false");
+        }
+    }
+});
